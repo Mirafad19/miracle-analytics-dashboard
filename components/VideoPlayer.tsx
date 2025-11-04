8ac7423e091185730c793482927f38f1dd931df6
unknown <fadahunsi.miracle@gmail.com>
2025-11-04 10:32:17 +0100

    FIX: New Changes Made

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -6,7 +6,7 @@ const VideoPlayer: React.FC = () => {
     <div className="relative aspect-video w-full max-w-4xl mx-auto rounded-2xl overflow-hidden shadow-2xl border-4 border-zinc-200 dark:border-zinc-800 bg-black">
       <iframe
         className="absolute top-0 left-0 w-full h-full"
-        src="https://www.youtube.com/embed/JCp-VxTNOwY?autoplay=1&mute=1&loop=1&playlist=JCp-VxTNOwY&controls=0&rel=0"
+        src="https://www.youtube.com/embed/JCp-VxTNOwY?controls=1&rel=0"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
